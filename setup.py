90f5a06dbaae7ca5c149d34248e568d7cd645908
Delaine Wendling <delaine@eventbrite.com>
2019-05-08 08:54:08 -0500

    Newest version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-version = '0.2.2'
+version = '0.5'
 
 install_requires = [
     'babel',
